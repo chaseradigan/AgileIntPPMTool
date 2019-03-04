3888d457aca4d25724a72af8950fb0f5cf5c7483
Chase Radigan <Chase.Radigan@hcl.com>
2019-03-03 16:43:15 -0800

    Custom Exceptions for Unique Project Id

--- PPMFullStackTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMFullStackTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -16,8 +17,12 @@ public class ProjectService {
 	
 	public Project saveOrUpdateProject(Project project) {
 		
-		//Logic
-		
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());;
+			return projectRepository.save(project);
+		}
+		catch(Exception e) {
+			throw new ProjectIdException("Project ID "+project.getProjectIdentifier().toUpperCase()+" already exists");
+		}
 	}
 }
