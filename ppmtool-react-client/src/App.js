f3c9238e382621829e2012b2e69dc936523b2155
Chase Radigan <Chase.Radigan@hcl.com>
2019-03-04 12:28:09 -0800

    Bringing Bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 class App extends Component {
   render() {
