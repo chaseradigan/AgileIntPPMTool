6790769a29df8f5ba86f57d32c68f1d93b132818
Chase Radigan <Chase.Radigan@hcl.com>
2019-03-05 13:40:15 -0800

    Project Object Updates

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
