7a74ce6a8bfcedfd2b1636ba2115512803131951
Chase Radigan <Chase.Radigan@hcl.com>
2019-03-05 12:05:06 -0800

    Get Projects

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -11,6 +11,8 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
@@ -22,7 +24,9 @@ class Dashboard extends Component {
 
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -41,6 +45,6 @@ const mapStateToProps = state => ({
 });
 
 export default connect(
-  null,
-  getProjects
+  mapStateToProps,
+  { getProjects }
 )(Dashboard);
