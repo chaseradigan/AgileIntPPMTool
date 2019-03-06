80b9b14654c5a2f5572825bb56f09ede8cb089a2
Chase Radigan <Chase.Radigan@hcl.com>
2019-03-06 10:31:24 -0800

    Bug Fix

--- PPMFullStackTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMFullStackTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -11,40 +11,39 @@ import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
 @Service
 public class ProjectTaskService {
 
+	@Autowired
+	private BacklogRepository backlogRepository;
 
-    @Autowired
-    private BacklogRepository backlogRepository;
+	@Autowired
+	private ProjectTaskRepository projectTaskRepository;
 
-    @Autowired
-    private ProjectTaskRepository projectTaskRepository;
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
+		// Exceptions: Project not found
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+		// PTs to be added to a specific project, project != null, BL exists
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+		// set the bl to pt
+		projectTask.setBacklog(backlog);
+		// we want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
+		Integer BacklogSequence = backlog.getPTSequence();
+		// Update the BL SEQUENCE
+		BacklogSequence++;
+		backlog.setPTSequence(BacklogSequence);
 
-        //Exceptions: Project not found
+		// Add Sequence to Project Task
+		projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
 
-        //PTs to be added to a specific project, project != null, BL exists
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        //set the bl to pt
-        projectTask.setBacklog(backlog);
-        //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
-        Integer BacklogSequence = backlog.getPTSequence();
-        // Update the BL SEQUENCE
-        BacklogSequence++;
+		// INITIAL priority when priority null
+		if (projectTask.getPriority() == null) { // in the future we need the projectTask.getPriority()==0
+			projectTask.setPriority(3);
+		}
+		// INITIAL status when status is null
+		if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+			projectTask.setStatus("TO_DO");
+		}
 
-        //Add Sequence to Project Task
-        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
-
-        //INITIAL priority when priority null
-//        if(projectTask.getPriority()==0||projectTask.getPriority()==null){
-//            projectTask.setPriority(3);
-//        }
-        //INITIAL status when status is null
-        if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
-    }
+		return projectTaskRepository.save(projectTask);
+	}
 }
