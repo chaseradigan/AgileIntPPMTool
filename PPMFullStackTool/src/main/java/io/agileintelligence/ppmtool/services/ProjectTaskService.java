bbe14e3d327a93364631f352aa44583ac33ee77b
Chase Radigan <Chase.Radigan@hcl.com>
2019-03-08 13:13:38 -0800

    update

--- PPMFullStackTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMFullStackTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.Backlog;
-import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.domain.ProjectTask;
 import io.agileintelligence.ppmtool.exceptions.ProjectNotFoundException;
 import io.agileintelligence.ppmtool.repositories.BacklogRepository;
@@ -23,45 +22,44 @@ public class ProjectTaskService {
 	@Autowired
 	private ProjectRepository projectRepository;
 
-	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+	@Autowired
+	private ProjectService projectService;
 
-		try {
-			// PTs to be added to a specific project, project != null, BL exists
-			Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-			// set the bl to pt
-			projectTask.setBacklog(backlog);
-			// we want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
-			Integer BacklogSequence = backlog.getPTSequence();
-			// Update the BL SEQUENCE
-			BacklogSequence++;
-			backlog.setPTSequence(BacklogSequence);
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask, String username) {
 
-			// Add Sequence to Project Task
-			projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
-			projectTask.setProjectIdentifier(projectIdentifier);
+		// PTs to be added to a specific project, project != null, BL exists
+		Backlog backlog = projectService.findProjectByIdentifier(projectIdentifier, username).getBacklog(); // backlogRepository.findByProjectIdentifier(projectIdentifier);
+		// set the bl to pt
+		System.out.println(backlog);
+		projectTask.setBacklog(backlog);
+		// we want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
+		Integer BacklogSequence = backlog.getPTSequence();
+		// Update the BL SEQUENCE
+		BacklogSequence++;
+		backlog.setPTSequence(BacklogSequence);
 
-			// INITIAL priority when priority null
-			if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) { // in the future we need the projectTask.getPriority()==0
-				projectTask.setPriority(3);
-			}
-			// INITIAL status when status is null
-			if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-				projectTask.setStatus("TO_DO");
-			}
+		// Add Sequence to Project Task
+		projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
 
-			return projectTaskRepository.save(projectTask);
-		} catch (Exception e) {
-			throw new ProjectNotFoundException("Project not found");
+		// INITIAL priority when status null
+		if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+			projectTask.setStatus("TO_DO");
 		}
+
+		// Fix bug with priority in Spring Boot Server, needs to check null first
+		if (projectTask.getPriority() == null || projectTask.getPriority() == 0) { // In the future we need
+																					// projectTask.getPriority()== 0 to
+																					// handle the form
+			projectTask.setPriority(3);
+		}
+
+		return projectTaskRepository.save(projectTask);
 	}
 
-	public Iterable<ProjectTask> findBacklogById(String id) {
+	public Iterable<ProjectTask> findBacklogById(String id, String username) {
 
-		Project project = projectRepository.findByProjectIdentifier(id);
-
-		if (project == null) {
-			throw new ProjectNotFoundException("Project with ID:" + id + " does not exist");
-		}
+		projectService.findProjectByIdentifier(id, username);
 
 		return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
 	}
